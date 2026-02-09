16859904bee8eeda525ef4fb5d6fa33d24de7ba2
JuanRamz20 <a01736533@tec.mx>
2026-02-09 15:50:52 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,10 +78,10 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dap37qceu/video/upload/v1770673776/Ghost_-_Life_Eternal_Audio_ycrfbm.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1                        // Volume level (0.0 to 1.0)
     }
 };
 
